3a5528a4fe3ecf24a79aead1c5c4744f0a6ed67b
rosewood28 <72754180+rosewood28@users.noreply.github.com>
2025-06-07 22:03:29 +0300

    Added correct antMemory size and capacity calculations

---
+++ src/mobemu/algorithms/GRACE.java
@@ -0,0 +1,298 @@
+package mobemu.algorithms;
+
+import mobemu.node.Context;
+import mobemu.node.Message;
+import mobemu.node.Node;
+import mobemu.utils.GraceHelper;
+
+import java.util.*;
+
+/**
+ * Class for a GRACE node.
+ *
+ * GRACE (Gradient Relays via Ant Colony Exploration) is a routing algorithm that uses
+ * lightweight control packets called ants to explore the network and establish
+ * gradient-based routes for messages.
+ */
+public class GRACE extends Node {
+
+    /**
+     * Initial ant TTL
+     */
+    private static final long INITIAL_ANT_TTL = 24 * 60 * 60 * 1000; //24 hours
+
+    /**
+     * Initial ant pheromone strength (with what value it will update
+     * the pheromone levels in the next node's pheromoneTable)
+     */
+    private static final double INITIAL_ANT_STRENGTH = 1;
+
+    /**
+     * Decay per tick
+     */
+    private static final double EVAPORATION_RATE = 0.1;
+
+    /**
+     * Minimum pheromone value to consider a node as a valid relay in the pheromone table.
+     */
+    private static final double MINIMUM_PHEROMONE_THRESHOLD = 0.3;
+
+    /**
+     * Size in units of a normal message used for bandwidth calculations.
+     */
+    public static final int GRACE_MESSAGE_SIZE = 5;
+
+    /**
+     * Size in units of an ant used for bandwidth calculations.
+     */
+    public static final int GRACE_ANT_SIZE = 1;
+
+    /**
+     * Pheromone levels indicating how frequently this node is visited by ants from various sources.
+     * Used to estimate how useful this node could be as a relay for messages originating from those sources.
+     */
+    private Map<Integer, Double> pheromoneTable = new HashMap<>();
+
+    /**
+     * Maximum size of ant memory in storage units.
+     */
+    private final int antMemoryCapacity;
+
+    /**
+     * Separate memory for ants
+     */
+    private List<GraceHelper.Ant> antMemory = new ArrayList<>();
+
+    /**
+     * Instantiates an {@code GRACE} object.
+     *
+     * @param id ID of the node
+     * @param nodes total number of existing nodes
+     * @param context the context of this node
+     * @param socialNetwork the social network as seen by this node
+     * @param antMemorySize maximum size of ant memory
+     * @param dataMemorySize maximum size of data memory
+     * @param exchangeHistorySize maximum size of exchange history
+     * @param seed seed for the random number generator
+     * @param traceStart timestamp of the start time of the trace
+     * @param traceEnd timestamp of the end time of the trace
+     */
+    public GRACE(int id, int nodes, Context context, boolean[] socialNetwork, int antMemorySize, int dataMemorySize, int exchangeHistorySize, long seed, long traceStart, long traceEnd) {
+        super(id, nodes, context, socialNetwork, dataMemorySize, exchangeHistorySize, seed, traceStart, traceEnd);
+
+        // Convert ant memory size to storage units
+        this.antMemoryCapacity = antMemorySize * GRACE_MESSAGE_SIZE;
+    }
+
+    @Override
+    public String getName() { return "GRACE"; }
+
+    @Override
+    protected void onDataExchange(Node encounteredNode, long contactDuration, long currentTime) {
+        // Check and cast to the GRACE instance
+        if (!(encounteredNode instanceof GRACE graceEncounteredNode)) {
+            return;
+        }
+
+        // Perform the delivery of messages that are destined for the current node
+        int remainingMessages = deliverDirectMessages(graceEncounteredNode, false, contactDuration, currentTime, false);
+
+        // Convert remainingMessages to bandwidth units (1 message = GRACE_MESSAGE_SIZE units)
+        double remainingCapacity = remainingMessages * GRACE_MESSAGE_SIZE;
+        double antCapacity = remainingCapacity * 0.3;
+        remainingCapacity -= antCapacity;
+        int usedCapacity = 0;
+
+        // First we allow ants to update the available pathways in order to take the best decision based on recent data
+        for (GraceHelper.Ant ant : graceEncounteredNode.antMemory) {
+            // Assure that network capacity is not exceeded
+            if (usedCapacity + GRACE_ANT_SIZE > antCapacity) {
+                break;
+            }
+
+            // Count successful ant transfers against bandwidth (each ant uses GRACE_ANT_SIZE bandwidth unit)
+            if (insertAnt(ant, graceEncounteredNode, currentTime)) {
+                usedCapacity += GRACE_ANT_SIZE;
+            }
+        }
+        // Reset used capacity to 0 for messages transfers capacity calculations
+        usedCapacity = 0;
+
+        // Analyze messages of the encountered node's data memory and apply algorithm
+        for (Message message : graceEncounteredNode.dataMemory) {
+            // Assure that network capacity is not exceeded
+            if (usedCapacity + GRACE_MESSAGE_SIZE > remainingCapacity) {
+                return;
+            }
+
+            // Count successful messages transfers against bandwidth (each message uses GRACE_MESSAGE_SIZE bandwidth unit)
+            if (shouldAcceptMessage(message, graceEncounteredNode)) {
+                if (insertMessage(message, graceEncounteredNode, currentTime, false, false)) {
+                    usedCapacity += GRACE_MESSAGE_SIZE;
+                }
+            }
+        }
+
+        // Analyze messages of the encountered node's own memory and apply algorithm
+        for (Message message : graceEncounteredNode.ownMessages) {
+            // Assure that network capacity is not exceeded
+            if (usedCapacity + GRACE_MESSAGE_SIZE > remainingCapacity) {
+                return;
+            }
+
+            // Count successful messages transfers against bandwidth (each message uses GRACE_MESSAGE_SIZE bandwidth unit)
+            if (shouldAcceptMessage(message, graceEncounteredNode)) {
+                if (insertMessage(message, graceEncounteredNode, currentTime, false, false)) {
+                    usedCapacity += GRACE_MESSAGE_SIZE;
+                }
+            }
+        }
+    }
+
+    /**
+     * Decides whether to accept a message based on the pheromone value of the message's source.
+     *
+     * @param message the message to accept
+     * @return {@code true} if the message should be accepted, {@code false} otherwise
+     */
+    private boolean shouldAcceptMessage(Message message, Node encounteredNode) {
+        // Get pheromone value for message source
+        double pheromoneValue = pheromoneTable.getOrDefault(message.getSource(), 0.0);
+        //double pheromoneValueDestination = pheromoneTable.getOrDefault(message.getDestination(), 0.0);
+
+        // if (pheromoneValueDestination > 0.1 || pheromoneValue > 0.1) {
+        //     //System.out.println("Message destination has pheromone value: " + pheromoneValueDestination);
+        //     return true;
+        // }
+
+        // Logic to decide whether to accept the message based on pheromone values.
+        return pheromoneValue > 0.1;
+    }
+
+    /**
+     * Inserts an ant into the ant memory.
+     * Ants travel by creating copies of the original ant when needed.
+     * If a node decides to tranfer an ant into it's own memory, it creates
+     * a new copy of the ant and adds it to its own memory. From this moment
+     * on, the two are separate entities that explore different paths in the network.
+     * It is allowed for a node to receive multiple ants that belong to the same
+     * message. This reflects the existence of multiple paths from the source
+     * to the node or the strength of a particular path that attracts many ants.
+     * However, a single ant copy is not allowed to visit the same node more than
+     * once in order to prevent loops.
+     *
+     * @param ant the ant to insert in antMemory
+     * @param from the node from which the ant is coming
+     * @param currentTime the current time
+     * @return {@code true} if the ant was transfered, {@code false} otherwise
+     */
+    private boolean insertAnt(GraceHelper.Ant ant, Node from, long currentTime) {
+        // Skip if ant is expired
+        if (ant.isExpired(currentTime)) {
+            return false;
+        }
+
+        // Create a new ant copy for this new path with a new copyId
+        GraceHelper.Ant newAnt = ant.clone();
+
+        // If this copy has already visited this node, reject to prevent loops
+        if (newAnt.getAntStats().hasVisited(this.id)) {
+            return false;
+        }
+
+        // Mark this node as visited by this copy
+        newAnt.getAntStats().markAsVisited(this.id, currentTime);
+
+        // Update pheromone values
+        double currentPheromone = pheromoneTable.getOrDefault(newAnt.getSource(), 0.0);
+        double newStrength = newAnt.computeStrength(currentTime);
+        pheromoneTable.put(newAnt.getSource(), currentPheromone + newStrength);
+
+        // If ant memory is full, remove the oldest ant
+        if (antMemoryCapacity != Integer.MAX_VALUE && antMemory.size() + GRACE_ANT_SIZE >= antMemoryCapacity) {
+            antMemory.remove(0);
+        }
+
+        // Add ant to antMemory
+        antMemory.add(newAnt);
+
+        return true;
+    }
+
+    /**
+     * At message generation, create an assigned ant for future exploration
+     *
+     * @param message the message to generate an ant for
+     * @return the generated ant
+     */
+    @Override
+    public Message generateMessage(Message message) {
+        // Create ant
+        GraceHelper.Ant ant = new GraceHelper.Ant(
+                message.getSource(),
+                message.getDestination(),
+                message.getId(),
+                message.getTimestamp(),
+                INITIAL_ANT_TTL,
+                INITIAL_ANT_STRENGTH
+        );
+
+        // Add ant in antMemory for future spreading in the network
+        antMemory.add(ant);
+
+        // Add GRACE message in own memory
+        ownMessages.add(message);
+
+        return message;
+    }
+
+    /**
+     * Updates the pheromone table and removes expired ants.
+     *
+     * @param currentTime the current time
+     * @param sampleTime the sample time
+     */
+    @Override
+    protected void onTick(long currentTime, long sampleTime) {
+        super.onTick(currentTime, sampleTime);
+
+        // Decay pheromone levels of all nodes
+        for (Map.Entry<Integer, Double> entry : pheromoneTable.entrySet()) {
+            pheromoneTable.put(entry.getKey(), entry.getValue() * (1 - EVAPORATION_RATE));
+        }
+
+        // Remove all expired ants
+        antMemory.removeIf(ant -> ant.isExpired(currentTime));
+
+        // Remove all node entries with pheromone value below the threshold
+        pheromoneTable.entrySet().removeIf(entry -> entry.getValue() < MINIMUM_PHEROMONE_THRESHOLD);
+    }
+
+    /**
+     * Checks if the ant memory of this node contains a specific ant.
+     *
+     * @param otherAnt the ant to check for
+     * @return {@code true} if the ant is in the memory, {@code false} otherwise
+     */
+    public boolean hasAnt(GraceHelper.Ant otherAnt) {
+        for (GraceHelper.Ant ant : antMemory) {
+            if (ant.equals(otherAnt)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void printPheromoneTable() {
+        if (pheromoneTable.isEmpty()) {
+            System.out.println("Pheromone table is empty.");
+            return;
+        }
+
+        System.out.println("Pheromone Table Contents:");
+        for (Map.Entry<Integer, Double> entry : pheromoneTable.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+    }
+}
---
+++ src/mobemu/utils/GraceHelper.java
@@ -0,0 +1,469 @@
+package mobemu.utils;
+
+import mobemu.node.Message;
+
+import java.util.*;
+
+/**
+ * Helper class for ant-based algorithms.
+ */
+public class GraceHelper {
+
+    /**
+     * Represents an ant in the GRACE algorithm. Ants are lightweight control packets
+     * that explore the network to establish gradient-based routes for messages.
+     * Each ant is associated with a message and it's source node and leaves
+     * pheromone traces as it moves through the network.
+     * These will travel like messages but are not considered messages since they serve a
+     * different purpose and should not skew the metrics and statistics Mobemu computes for those.
+     */
+    public static class Ant {
+
+        /**
+         * Base for the pheromone strength decay factor.
+         */
+        private static final double DECAY_FACTOR_BASE = 1000.0 * 60 * 60 * 2;
+
+        /**
+         * Id of the ant.
+         */
+        private final int antId;
+
+        /**
+         * Id of the copy of the ant.
+         */
+        private int copyId;
+
+        /**
+         * Id of the origin node of the ant.
+         */
+        private final int source;
+
+        /**
+         * Id of the destination of the message and implicitely the node these ants
+         * are looking for.
+         */
+        private final int destination;
+
+        /**
+         * Id of the message this ant was generated for.
+         */
+        private final int messageID;
+
+        /**
+         * Time when the ant was generated.
+         */
+        private final long timestamp;
+
+        /**
+         * Time-to-live of an ant (how long before expiration).
+         */
+        private long ttl;
+
+        /**
+         * The value this ant will use to update the pheromones of the next node it will visit.
+         * This values decreases with every hop since the further we get fromm the source the more
+         * reasons to having the path back already interrupted or changed.
+         */
+        private double strength;
+
+        /**
+         * Statistics of this ant.
+         */
+        private AntStats antStats;
+
+        /**
+         * Total number of ants generated.
+         */
+        private static int antCount = 0;
+
+        /**
+         * Counter for copies of the ant
+         */
+        private static int copyCount = 0;
+
+        /**
+         * Constructor for a {@link Ant} object.
+         *
+         * @param source          the sender of the message that generated the ant
+         * @param destination     the destination of the message and the nodes the ants are looking for
+         * @param timestamp       time of the ant generation
+         * @param initialTTL      time-to-live of the ant
+         * @param initialStrength initial strength of the ant
+         */
+        public Ant(int source, int destination, int messageID, long timestamp, long initialTTL, double initialStrength) {
+            this.antId = antCount++;
+            this.copyId = 0;
+            this.source = source;
+            this.destination = destination;
+            this.messageID = messageID;
+            this.timestamp = timestamp;
+            this.ttl = initialTTL;
+            this.strength = initialStrength;
+            // Every ant start with one copy and creates a copy only when needed
+            this.antStats = new AntStats(1, source);
+        }
+
+        // Private constructor for cloning
+        private Ant(int antId, int copyId, int source, int destination, int messageID,
+                    long timestamp, long ttl, double strength, AntStats antStats) {
+            this.antId = antId;
+            this.copyId = copyId;
+            this.source = source;
+            this.destination = destination;
+            this.messageID = messageID;
+            this.timestamp = timestamp;
+            this.ttl = ttl;
+            this.strength = strength;
+            this.antStats = antStats;
+        }
+        /**
+         * Clones the ant. Ant stats are also cloned allowing every ant (either generated at
+         * message creation or through cloning) to track its own path without losing the
+         * original ant's history.
+         *
+         * @return the cloned ant
+         */
+        public Ant clone() {
+            return new Ant(
+                    this.antId,
+                    ++copyCount,
+                    this.source,
+                    this.destination,
+                    this.messageID,
+                    this.timestamp,
+                    this.ttl,
+                    this.strength,
+                    this.antStats.clone(this.source)  // Clone the stats of original ant
+            );
+        }
+
+        /**
+         * Computes pheromone strength this ant will add to the next node it visits based on elapsed time
+         * since creation of the ant.
+         *
+         * @param currentTime Current simulation time.
+         * @return Decayed strength (exponential decay in this case
+         */
+        public double computeStrength(long currentTime) {
+            long elapsed = currentTime - timestamp;
+            if (elapsed > this.ttl) return 0.0;
+
+//            System.out.println("=== Strength Computation ===");
+//            System.out.println("Current time: " + currentTime);
+//            System.out.println("Timestamp: " + timestamp);
+//            System.out.println("Elapsed: " + elapsed);
+//            System.out.println("TTL: " + ttl);
+//            System.out.println("Decay factor: " + Math.exp(-elapsed / DECAY_FACTOR_BASE));
+//            System.out.println("Initial strength: " + this.strength);
+
+            // Exponential decay
+            double decayFactor = Math.exp(-elapsed / DECAY_FACTOR_BASE);
+            double finalStrength = this.strength * decayFactor;
+
+
+            //System.out.println("Final strength: " + finalStrength);
+            return finalStrength;
+        }
+
+        /**
+         * Updates the strength of the ant.
+         *
+         * @param newStrength new strength of the ant
+         */
+        public void updateStrength(double newStrength) {
+            this.strength = newStrength;
+        }
+
+        /**
+         * Checks if the current ant is expired or not
+         *
+         * @param currentTime Current simulation time.
+         * @return {@code true} if expired or {@code false} if not
+         */
+        public boolean isExpired(long currentTime) {
+            return (currentTime - timestamp) > ttl;
+        }
+
+        /**
+         * Checks if two ants are equal.
+         *
+         * @param o the object to compare
+         * @return {@code true} if equal, {@code false} otherwise
+         */
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Ant ant = (Ant) o;
+            return antId == ant.antId && copyId == ant.copyId;
+        }
+
+        /**
+         * Gets the hash code of the ant.
+         *
+         * @return the hash code of the ant
+         */
+        @Override
+        public int hashCode() {
+            return Objects.hash(antId, copyId);
+        }
+
+        /**
+         * Gets the id of the ant.
+         *
+         * @return the id of the ant
+         */
+        public int getAntId() {
+            return antId;
+        }
+
+        /**
+         * Gets the id of the copy of the ant.
+         *
+         * @return the id of the copy of the ant
+         */
+        public int getCopyId() {
+            return copyId;
+        }
+
+        /**
+         * Gets the source id of the ant.
+         *
+         * @return the source of the ant
+         */
+        public int getSource() {
+            return source;
+        }
+
+        /**
+         * Gets the destination id of the ant.
+         *
+         * @return the destination of the ant
+         */
+        public int getDestination() {
+            return destination;
+        }
+
+        /**
+         * Gets the id of the message this ant was generated for.
+         *
+         * @return the id of the message
+         */
+        public int getMessageID() {
+            return messageID;
+        }
+
+        /**
+         * Gets the timestamp when the ant was generated.
+         *
+         * @return the timestamp of the ant generation
+         */
+        public long getTimestamp() {
+            return timestamp;
+        }
+
+        /**
+         * Gets the time-to-live of the ant.
+         *
+         * @return the time-to-live of the ant
+         */
+        public long getTtl() {
+            return ttl;
+        }
+
+        /**
+         * Gets the strength this ant will update
+         * the future encountered node's pheromone value.
+         *
+         * @return the strength of the ant
+         */
+        public double getStrength() {
+            return strength;
+        }
+
+        /**
+         * Gets the statistics of the ant.
+         *
+         * @return the statistics of the ant
+         */
+        public AntStats getAntStats() {
+            return antStats;
+        }
+
+    }
+
+    /**
+     * Class that tracks the statistics of an ant.
+     */
+    public static class AntStats {
+        /**
+         * Number of hops an ant has until now per node.
+         */
+        private Map<Integer, Integer> hops;
+
+        /**
+         * Number of copies of this ant at each node.
+         */
+        private Map<Integer, Integer> copies;
+
+        /**
+         * List of nodes this ant has visited.
+         */
+        private Set<Integer> visited;
+
+        /**
+         * When each node was visited.
+         */
+        private Map<Integer, Long> visitTime;
+
+        /**
+         * Constructor for a {@link AntStats} object.
+         *
+         * @param initialCopies number of initial copies
+         * @param sourceId      ID of the node that generates the ant
+         */
+        public AntStats(int initialCopies, int sourceId) {
+            this.copies = new HashMap<>();
+            this.copies.put(sourceId, initialCopies);   // Initial copies at source node
+            this.hops = new HashMap<>();
+            this.visited = new HashSet<>();
+            this.visitTime = new HashMap<>();
+        }
+
+        /**
+         * Gets the number of copies of this ant at a given node.
+         *
+         * @param nodeId ID of the node
+         * @return number of copies
+         */
+        public int getCopies(int nodeId) {
+            Integer result = copies.get(nodeId);
+            return result == null ? 0 : result;
+        }
+
+        /**
+         * Sets a new value for the number of copies of an ant at a given node.
+         *
+         * @param nodeId ID of the node
+         * @param value  new value for copies
+         */
+        public void setCopies(int nodeId, int value) {
+            copies.put(nodeId, value);
+        }
+
+        /**
+         * Deletes all copies of this ant at the given node.
+         *
+         * @param nodeId ID of the node from which the ant is deleted
+         */
+        public void deleteCopies(int nodeId) {
+            if (copies.containsKey(nodeId)) {
+                copies.put(nodeId, 0);
+            }
+        }
+
+        /**
+         * Duplicates the copies of an ant from a given node to another one.
+         *
+         * @param from source node ID
+         * @param to   destination node ID
+         */
+        public void copy(int from, int to) {
+            copies.put(to, copies.get(from));
+        }
+
+        /**
+         * Marks a node as visited by this ant.
+         *
+         * @param nodeId         ID of the node visited
+         * @param visitTimestamp timestamp of the visit
+         */
+        public void markAsVisited(int nodeId, long visitTimestamp) {
+            visited.add(nodeId);
+            increaseHopCount(nodeId);
+            visitTime.put(nodeId, visitTimestamp);
+        }
+
+        /**
+         * Increases the number of hops an ant has.
+         * Can also be seen as a path that registers every node and
+         * the number of hops an ant had until visiting that node.
+         *
+         * @param nodeId ID of the node
+         * @return new number of hops
+         */
+        public int increaseHopCount(int nodeId) {
+            Integer hopsCount = hops.get(nodeId);
+            if (hopsCount == null) {
+                hopsCount = 0;
+            }
+            hopsCount++;
+            hops.put(nodeId, hopsCount);
+            return hopsCount;
+        }
+
+        /**
+         * Checks if the ant has visited a given node.
+         *
+         * @param nodeId ID of the node
+         * @return {@code true} if visited, {@code false} otherwise
+         */
+        public boolean hasVisited(int nodeId) {
+            return visited.contains(nodeId);
+        }
+
+        /**
+         * Clones the ant stats.
+         *
+         * @return the cloned ant stats
+         */
+        public AntStats clone(int source) {
+            AntStats clone = new AntStats(1, source);  // Create new base stats
+
+            // Copy all the tracking information
+            clone.hops = new HashMap<>(this.hops);
+            clone.copies = new HashMap<>(this.copies);
+            clone.visited = new HashSet<>(this.visited);
+            clone.visitTime = new HashMap<>(this.visitTime);
+
+            return clone;
+        }
+
+        /**
+         * Gets the number of hops the ant has until now per node.
+         *
+         * @return the number of hops
+         */
+        public Map<Integer, Integer> getHops() {
+            return hops;
+        }
+
+        /**
+         * Gets the number of copies of the ant at each node.
+         *
+         * @return the number of copies
+         */
+        public Map<Integer, Integer> getCopies() {
+            return copies;
+        }
+
+        /**
+         * Gets the nodes visited by the ant.
+         *
+         * @return the nodes visited
+         */
+        public Set<Integer> getVisited() {
+            return visited;
+        }
+
+        /**
+         * Gets the time when each node was visited by the ant.
+         *
+         * @return the time when each node was visited
+         */
+        public Map<Integer, Long> getVisitTime() {
+            return visitTime;
+        }
+    }
+}
